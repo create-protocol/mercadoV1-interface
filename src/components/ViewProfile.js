349d61ae43abd061e66dba1a41dc3f8c1dfb4b43
Aman Gora <49208239+Nicklaus01@users.noreply.github.com>
2021-10-30 18:48:57 +0530

    ft: my nft image layout change

--- src/components/ViewProfile.js
+++ src/components/ViewProfile.js
@@ -55,12 +55,14 @@ const Viewprofile = () => {
   if (loadingState === 'loaded' && !nfts.length) return (<h1 className="py-10 px-20 text-3xl">No assets owned</h1>)
   return (
     <div className="flex justify-center">
-      <div className="p-4">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+      <div className=" my-4 ml-4">
+        <div className="m-card-content">
           {
             nfts.map((nft, i) => (
-              <div key={i} className="border shadow rounded-xl overflow-hidden">
-                <img src={nft.image} className="" />
+              <div key={i} className="nft-card-container m-2">
+                <div className="nft-img-container">
+                <img src={nft.image} className="nft-img" />
+                </div>
                 <div className="p-4 bg-black">
                   <p className="text-2xl font-bold text-white">Price - {nft.price} Eth</p>
                 </div>
