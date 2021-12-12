d31cb836efd9e6035517a05e4e9c06f14188ed05
shaurya127 <shaurya1272000@gmail.com>
2021-12-12 16:48:26 +0530

    integrating contract's v1

--- src/components/NFTsList.js
+++ src/components/NFTsList.js
@@ -2,42 +2,39 @@ import React from "react";
 import { ethers } from "ethers";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import Web3Modal from "web3modal"
+import Web3Modal from "web3modal";
 import "../assets/css/nft.css";
 import Market from "../abis/Marketplace.json";
 import NFT from "../abis/NFT.json";
 import { Link } from "react-router-dom";
-// import spinloader from './spinloader';
-// import Spinner from 'react-bootstrap/Spinner'
 import { useHistory } from "react-router";
 import styled from "styled-components";
 import { nftmarketaddress, nftaddress } from "../config";
 import Loader from "react-loader-spinner";
 const ShadowBtn = styled.div`
-background-color: rgb(112, 215, 49);
-color: rgb(26, 24, 24);
-font-size: 20px;
-font-weight: 700;
-width:40%;
-border: 10px solid rgb(48, 52, 57);
-border-radius: 20px;
-padding:6px 10px 6px 10px;
-cursor: pointer;
-margin-top: 0.625rem;
-max-width: 900px;
-transition: all 0.3s ease-in-out 0s;
-box-shadow: rgb(53 54 56 / 50%) 0px 16px 30px;
-margin-top:40px;
-margin-right: 20px;
-margin-left: 20px;
-  &:hover{
+  background-color: rgb(112, 215, 49);
+  color: rgb(26, 24, 24);
+  font-size: 20px;
+  font-weight: 700;
+  width: 40%;
+  border: 10px solid rgb(48, 52, 57);
+  border-radius: 20px;
+  padding: 6px 10px 6px 10px;
+  cursor: pointer;
+  margin-top: 0.625rem;
+  max-width: 900px;
+  transition: all 0.3s ease-in-out 0s;
+  box-shadow: rgb(53 54 56 / 50%) 0px 16px 30px;
+  margin-top: 40px;
+  margin-right: 20px;
+  margin-left: 20px;
+  &:hover {
     -webkit-box-shadow: 0 0 8px #fff;
-        box-shadow: 0 0 8px #fff;
-        transition:.5s;
-        border-radius:20px
+    box-shadow: 0 0 8px #fff;
+    transition: 0.5s;
+    border-radius: 20px;
   }
 `;
-
 
 const Nftslist = (props) => {
   const [nfts, setNfts] = useState([]);
@@ -48,9 +45,8 @@ const Nftslist = (props) => {
   }, []);
   const history = useHistory();
   async function loadNFTs() {
-   
     const provider = new ethers.providers.JsonRpcProvider(
-      `https://eth-kovan.alchemyapi.io/v2/-rsx-HZE8Py7I7mOMIRCHckg3ab-xKnU`
+      `https://polygon-mumbai.g.alchemy.com/v2/T-sMRF2J8t9nSNy7dBwBFNijlNhhk1ij`
     );
 
     const marketContract = new ethers.Contract(
@@ -60,7 +56,7 @@ const Nftslist = (props) => {
     );
     const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider);
 
-    const data = await marketContract.fetchMarketItems();
+    const data = await marketContract.fetchAllNFTs();
     console.log(data);
     const items = await Promise.all(
       data.map(async (i) => {
@@ -75,82 +71,83 @@ const Nftslist = (props) => {
           sold: i.sold,
           image: meta.data.image,
           desc: meta.data.description,
-          collection:meta.data.collection
+          collection: meta.data.collection,
         };
         return item;
       })
     );
     console.log("Token listed for sale.");
-    // console.log(items);
-    /* create a filtered array of items that have been sold */
     const soldItems = items.filter((i) => i.sold);
     setSold(soldItems);
     setNfts(items);
     setLoadingState("loaded");
-    console.log(items)
+    console.log(items);
   }
-
 
   async function buyNft(nft) {
-    const web3Modal = new Web3Modal()
-    const connection = await web3Modal.connect()
-    
-    const provider = new ethers.providers.Web3Provider(connection)
-    const signer = provider.getSigner()
-    const contract = new ethers.Contract(nftmarketaddress, Market.abi, signer)
-    console.log(nft)
-    const price = ethers.utils.parseUnits(nft.price, 'ether')
-    console.log(nftaddress)
-    console.log(nft.itemId)
-    const transaction = await contract.createMarketSale(nftaddress, nft.tokenId
-      , {value: price}
-      )
-    await transaction.wait()
-    loadNFTs()
-  }
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
 
-  // if(my component) else if else 
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(nftmarketaddress, Market.abi, signer);
+    console.log(nft);
+    const price = ethers.utils.parseUnits(nft.price, "ether");
+    console.log(nftaddress);
+    console.log(nft.itemId);
+    const transaction = await contract.createMarketSale(
+      nftaddress,
+      nft.tokenId,
+      { value: price }
+    );
+    await transaction.wait();
+    loadNFTs();
+  }
 
   if (loadingState != "loaded") {
-
-    return <div style={{ height: "200px", alignContent: "center", marginTop: "60px" }}>
-
-      <Loader
-        type="Puff"
-        color="#00BFFF"
-        height={100}
-        width={100}
-      // timeout={3000} //3 secs
-      />
-
-    </div>;
-  }
-
-  else if (loadingState === "loaded" && !nfts.length)
-    return <h1 className="py-10 px-20 text-3xl" style={{color:"white"}}>No assets created</h1>;
+    return (
+      <div
+        style={{ height: "200px", alignContent: "center", marginTop: "60px" }}
+      >
+        <Loader type="Puff" color="#00BFFF" height={100} width={100} />
+      </div>
+    );
+  } else if (loadingState === "loaded" && !nfts.length)
+    return (
+      <h1 className="py-10 px-20 text-3xl" style={{ color: "white" }}>
+        No assets created
+      </h1>
+    );
   return (
     <div>
       <div className="p-4">
         <div className=" my-4 ml-4 ">
           <div className="m-card-content" style={{ justifyContent: "center" }}>
             {nfts.map((nft, i) => (
-
-                <div key={i} className="row nft-card-container m-2" style={{display:"flex",flexDirection:"column",background:"grey",paddingBottom:"10px"}}>
-                  <Link
-                to={{
-                  pathname: "/descpage",
-                  state: {
-                    image: nft.image,
-                    name: nft.owner,
-                    price: nft.price,
-                    sellername: nft.seller,
-                    desc: nft.desc,
-                    collection:nft.collection
-                  },
+              <div
+                key={i}
+                className="row nft-card-container m-2"
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  background: "grey",
+                  paddingBottom: "10px",
                 }}
               >
+                <Link
+                  to={{
+                    pathname: "/descpage",
+                    state: {
+                      image: nft.image,
+                      name: nft.owner,
+                      price: nft.price,
+                      sellername: nft.seller,
+                      desc: nft.desc,
+                      collection: nft.collection,
+                    },
+                  }}
+                >
                   <div className="nft-img-container">
-                  
                     <img className="nft-img" src={nft.image} alt="logo"></img>
                     <p
                       style={{
@@ -159,26 +156,31 @@ const Nftslist = (props) => {
                         color: "white",
                         textAlign: "start",
                         display: "flex",
-                        justifyContent: "space-between"
+                        justifyContent: "space-between",
                       }}
-
                     >
                       <div>{nft.price} Eth</div>
                       <div>{nft.collection}</div>
 
-                      <div><span
-                      //  style={{padding:"20px",marginRight:"9rem",textAlign:"end",justifyContent:"end",textAlign:"end"}}
-                      >{nft.seller.substring(0, 6) + "........." + nft.seller.slice(-3)}</span></div>
-
-
+                      <div>
+                        <span>
+                          {nft.seller.substring(0, 6) +
+                            "........." +
+                            nft.seller.slice(-3)}
+                        </span>
+                      </div>
                     </p>
-                    
-        
                   </div>
-                  </Link>
-                  <ShadowBtn style={{marginTop:"0"}} onClick={()=>buyNft(nft)}>Buy</ShadowBtn>
-                </div>
-              
+                </Link>
+
+                <ShadowBtn
+                  style={{ marginTop: "0" }}
+                  onClick={() => buyNft(nft)}
+                >
+                  Buy
+                </ShadowBtn>
+              </div>
+
               // </div>
             ))}
           </div>
@@ -194,7 +196,16 @@ const Nftslist = (props) => {
                   key={i}
                   className="border shadow rounded-xl overflow-hidden"
                 >
-                  <img style={{ width: "20rem", height: "20rem", objectFit: "contain" }} src={nft.image} className="rounded" alt="" />
+                  <img
+                    style={{
+                      width: "20rem",
+                      height: "20rem",
+                      objectFit: "contain",
+                    }}
+                    src={nft.image}
+                    className="rounded"
+                    alt=""
+                  />
                   <div className="p-4 bg-black">
                     <p className="text-2xl font-bold text-red">
                       Price - {nft.price} Eth
@@ -208,8 +219,6 @@ const Nftslist = (props) => {
       </div>
     </div>
   );
-
-  // 
 };
 
-export default Nftslist
+export default Nftslist;
--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@
 //export const nftmarketaddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
 //export const nftaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
 
-export const nftmarketaddress = "0xD194290D771c9689C16011192a0a5E6E70e4cab1"
-export const nftaddress = "0x93E11DA60102501c24546bC2F7D9456E241874B4"
+export const nftmarketaddress = "0x55D4A767FD1f81aF0b4C786c6bA73893dBE8771e"
+export const nftaddress = "0x44D17ceaE6E3a96bCB18dA8F0E87518beEFBE898"
+export const polygonweth="0x062f24cb618e6ba873ec1c85fd08b8d2ee9bf23e"
 
