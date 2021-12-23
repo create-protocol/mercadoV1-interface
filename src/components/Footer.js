a6618b5d18422d7779aad6404c117d90e780fe9f
shaurya127 <shaurya1272000@gmail.com>
2021-12-24 00:28:10 +0530

    mailchimp removed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,50 +16,6 @@ const Footer = () => {
   return (
     <Containertop>
 
-<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7_dtp.css" rel="stylesheet" type="text/css"/>
-<style type="text/css">
-	{/* #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; } */}
-	{/* /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block. */}
-	   {/* We recommend moving this block and the preceding CSS link to the HEAD of your HTML file.  */}
-</style>
-<div id="mc_embed_signup">
-<form action="https://createprotocol.us20.list-manage.com/subscribe/post?u=d6b2d0267ff4af5f6f784edc2&amp;id=f3a79bcd16" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-    <div id="mc_embed_signup_scroll">
-	<h2>Subscribe</h2>
-<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-<div class="mc-field-group">
-	<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
-</label>
-	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL"/>
-</div>
-<div class="mc-field-group">
-	<label for="mce-FNAME">First Name </label>
-	<input type="text" value="" name="FNAME" class="" id="mce-FNAME"/>
-</div>
-<div class="mc-field-group">
-	<label for="mce-LNAME">Last Name </label>
-	<input type="text" value="" name="LNAME" class="" id="mce-LNAME"/>
-</div>
-<div class="mc-field-group size1of2">
-	
-		
-		
-		
-	</div>
-</div>	<div id="mce-responses" class="clear foot">
-		<div class="response" id="mce-error-response" style="display:none"></div>
-		<div class="response" id="mce-success-response" style="display:none"></div>
-	</div>   
-    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_d6b2d0267ff4af5f6f784edc2_f3a79bcd16" tabindex="-1" value=""/></div>
-        <div class="optionalParent">
-            <div class="clear foot">
-                <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"/>
-               
-            </div>
-        </div>
-        </form>
-
-    </div>
 
 
 
