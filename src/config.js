607fb123b46bf8c93fbcb8ee96e1e56c4d17c0ff
smrpn <samarpan_d@pp.iitr.ac.in>
2021-12-24 15:10:26 +0530

    ft: changed weth addr
    Signed-off-by: smrpn <samarpan_d@pp.iitr.ac.in>

--- src/config.js
+++ src/config.js
@@ -4,5 +4,5 @@
 
 export const nftmarketaddress = "0xed7ffF856756cd27fee101D486f23553e8e24EaA"
 export const nftaddress = "0x5d10fD561A8Ff2fb1ad67dC209fFB49838EBD63B"
-export const polygonweth = "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f61"
+export const polygonweth = "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619"
 
