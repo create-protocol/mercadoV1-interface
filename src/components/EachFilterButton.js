3bb5d7addaf96cbf053a8c61ab8e1a66121e06bf
shaurya127 <shaurya1272000@gmail.com>
2022-01-26 23:19:57 +0530

    pulling

--- src/components/EachFilterButton.js
+++ src/components/EachFilterButton.js
@@ -1,12 +1,8 @@
-import React, { useState, useEffect } from 'react'
-import arrow_down from '../assets/images/arrow_down.svg'
-import 'aos/dist/aos.css';
+import React, { useState, useEffect } from "react";
+
+import "aos/dist/aos.css";
 const EachFilterButton = ({ name, url }) => {
+  return <a href={`${url}`}>{name}</a>;
+};
 
-
-    return (
-        <a href={`${url}`}>{name}</a>
-    )
-}
-
-export default EachFilterButton
+export default EachFilterButton;
