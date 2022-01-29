a102ed5decfa38278af02ae326a2da9e37e749a7
salt-n-ice <2019UEE1465@mnit.ac.in>
2022-01-30 00:24:26 +0530

    price range updated

--- src/components/FilterComponent/Pricerange.js
+++ src/components/FilterComponent/Pricerange.js
@@ -5,12 +5,23 @@ const Pricerange = ({ label }) => {
     <div className="pricerange">
       <div className="counter">
         <label for="quantity">Lowest</label><br />
-        <input type="number" id="quantity" name="quantity" min="1" max="100" />
-        <div className="downbtn"></div>
+        <div style={{ display: "flex", height: "2rem" }}>
+          <input type="text" value={0} />
+          <div style={{ width: "10px", marginTop: "-10px" }}>
+            <div className="upbtn" style={{ marginBottom: "-15px" }}></div>
+            <div className="downbtn"></div>
+          </div>
+        </div>
       </div>
       <div className="counter">
         <label for="quantity">Highest</label><br />
-        <input type="number" id="quantity" name="quantity" min="1" max="100" />
+        <div style={{ display: "flex", height: "2rem" }}>
+          <input value={1000} />
+          <div style={{ width: "10px", marginTop: "-10px" }}>
+            <div className="upbtn" style={{ marginBottom: "-15px" }}></div>
+            <div className="downbtn"></div>
+          </div>
+        </div>
       </div>
     </div>
   );
