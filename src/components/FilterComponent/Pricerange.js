68faf03804ec62e0839acd852accc6616bb5de41
salt-n-ice <2019UEE1465@mnit.ac.in>
2022-01-30 00:04:17 +0530

    bc

--- src/components/FilterComponent/Pricerange.js
+++ src/components/FilterComponent/Pricerange.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./Pricerange.css";
-
 const Pricerange = ({ label }) => {
   return (
     <div className="pricerange">
-      <label for="quantity">Lowest</label>
-      <input type="number" id="quantity" name="quantity" min="1" max="100" />
-      <label for="quantity">Highest</label>
-      <input type="number" id="quantity" name="quantity" min="1" max="5" />
+      <div className="counter">
+        <label for="quantity">Lowest</label><br />
+        <input type="number" id="quantity" name="quantity" min="1" max="100" />
+        <div className="downbtn"></div>
+      </div>
+      <div className="counter">
+        <label for="quantity">Highest</label><br />
+        <input type="number" id="quantity" name="quantity" min="1" max="100" />
+      </div>
     </div>
   );
 };
