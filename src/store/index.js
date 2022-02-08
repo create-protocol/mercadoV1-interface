393ebb93307cf72ccf68379c3a590909c13af04f
pranjal1127 <2018kucp1127@iiitkota.ac.in>
2022-02-08 17:29:21 +0530

    Create Store for redux

---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { walletReducer } from "./wallet/walletReducer";
+import thunk from "redux-thunk"
+
+export * from './wallet';
+
+const reducers = combineReducers({
+  wallet : walletReducer,
+})
+
+export const store = createStore(reducers,{},applyMiddleware(thunk));
+
+
---
+++ src/store/wallet/walletReducer.js
@@ -0,0 +1,42 @@
+import {
+  WALLET_LOADING,
+  TOGGLE_POPUP,
+  ERROR,
+  UPDATE_WALLET
+} from "./walletAction";
+
+const initialState = {
+  wallet: null,
+  isWalletModelOpen: false,
+  isLoading : false,
+  Error : null,
+};
+
+export const walletReducer = (state = initialState, action) => {
+  // eslint-disable-next-line default-case
+  switch (action.type) {
+    case TOGGLE_POPUP:
+      return {
+        ...state,
+        isWalletModelOpen: !state.isWalletModelOpen,
+      };
+    case WALLET_LOADING:
+      return {
+        ...state,
+        isLoading: !state.isLoading,
+      };
+    case ERROR:
+      return {
+        ...state,
+        Error: action.ErrorText
+      }
+    case UPDATE_WALLET:
+      return {
+        ...state,
+        wallet: action.wallet,
+        isWalletModelOpen: false,
+      }
+    
+  }
+  return state;
+};
